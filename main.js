35a394c93f2358f96413f0801594e666d1be90cc
Yurii <87637869+CherniakYura@users.noreply.github.com>
2021-11-01 19:34:40 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,89 @@
+const APIkey = "d6714e1261b7b6e1059ba5bc1f266f6b";
+const city = document.querySelector(".city");
+const country = document.querySelector("#country");
+const weather = document.querySelector(".weather");
+const search = document.querySelector("#search");
+const find = document.querySelector("#searchContainer button");
+const tempType = document.querySelector("#tempType");
+const temp = document.querySelector("#temp");
+const desc = document.querySelector("#desc");
+const C = document.querySelector("#C");
+const F = document.querySelector("#F");
+const feelslike = document.querySelector("#feelslike");
+const humidity = document.querySelector("#humidity");
+const wind = document.querySelector("#wind");
+const pressure = document.querySelector("#pressure");
+const visibility = document.querySelector("#visibility");
+
+
+let curTemp = "metric";
+
+search.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        getWeather(search.value);
+        search.value = "";
+    }
+});
+
+find.addEventListener("click", function () {
+    getWeather(search.value);
+    search.value = "";
+});
+
+tempType.addEventListener("click", function () {
+    if (C.classList.contains("curTemp")) {
+        C.classList.remove("curTemp");
+        F.classList.add("curTemp");
+        curTemp = "imperial";
+        getWeather(city.textContent);
+    } else {
+        F.classList.remove("curTemp");
+        C.classList.add("curTemp");
+        curTemp = "metric";
+        getWeather(city.textContent);
+    }
+});
+
+async function getWeather(city) {
+    try {
+        let response = await fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" +
+                city +
+                "&units=" +
+                curTemp +
+                "&appid=" +
+                APIkey,
+            { mode: "cors" }
+        );
+        try {
+            let data = await response.json();
+            console.log(data);
+            fillPage(data);
+        } catch (err) {
+            alert("City not found");
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getWeather("London");
+
+function fillPage(data) {
+    city.textContent = data.name;
+    country.textContent = data.sys.country;
+    //+','+data.sys.country;
+    weather.textContent = data.weather[0].main;
+    temp.textContent = Math.round(data.main.temp);
+    desc.textContent = `Today: ${
+        data.weather[0].description
+    }. The high will be ${Math.round(
+        data.main.temp_max
+    )}°. The low tonight will be ${Math.round(data.main.temp_min)}°.`;
+
+    feelslike.textContent = Math.round(data.main.feels_like) + "°";
+    humidity.textContent = data.main.humidity + "%";
+        wind.textContent = Math.round(data.wind.speed) + " km/hr";
+        pressure.textContent=data.main.pressure +" hPa";
+        visibility.textContent =data.visibility/100 +" km";
+}
